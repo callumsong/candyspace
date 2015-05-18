cb814394e9d27616ed9605245bfa66dc483bbb3e
callumsong <ghksong@gmail.com>
2015-05-18 11:06:41 +0100

    server added

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(3000, function() {
+  console.log('sever listening on port ' + 3000);
+});
